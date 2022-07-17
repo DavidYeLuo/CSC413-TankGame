5e331ecb98ab80cf19a513747fd0d31efe1b4b4b
DavidYeLuo <davidayeluo@gmail.com>
2022-07-16 23:31:55 -0700

    Added documentation for the third person camera script.

--- TankGame/Assets/Scripts/Gameplay/Camera/ThirdPersonCamera.cs
+++ TankGame/Assets/Scripts/Gameplay/Camera/ThirdPersonCamera.cs
@@ -12,13 +12,12 @@ namespace Gameplay.Camera
         [SerializeField] private float distance;
         [SerializeField] private float angleFromZPlane;
 
-        [SerializeField] private Vector3 relativeLocation;
-
-        [Header("Debug")] [SerializeField] private bool objHasDifferentLocation;
+        [Header("Debug")]
+        [SerializeField] private bool objHasDifferentLocation; // If the stare obj implements IobjTransform or other.
         private IObjTransform objLocation;
-
+        
+        [SerializeField] private Vector3 relativeLocation;
         [SerializeField] private Vector3 objLoc;
-        [SerializeField] private Quaternion objRot;
 
         private void Start()
         {
@@ -37,33 +36,41 @@ namespace Gameplay.Camera
             relativeLocation *= distance;
         }
 
-        private void FixedUpdate()
+        private void LateUpdate()
         {
             UpdateRelativeLocation();
             UpdateObjLoc();
             UpdateCameraPosition();
             
-            // UpdateObjRot();
             UpdateCamRotation();
         }
 
+        /**
+         * Try to look at the objectToStare in the direction that it is looking
+         * This method deals with calculating where to place the current object relative to objectToStare.
+         */
         private void UpdateRelativeLocation()
         {
             if (objHasDifferentLocation)
             {
-                Transform objTransform = objLocation.GetTransform();
+                Transform objTransform = objLocation.GetTransform(); // Get the most recent location
                 relativeLocation = objTransform.forward * Mathf.Cos((angleFromZPlane + 180) * Mathf.Deg2Rad);
                 relativeLocation += Vector3.up * Mathf.Sin(angleFromZPlane * Mathf.Deg2Rad);
-                // relativeLocation = relativeLocation.normalized;
                 relativeLocation *= distance;
             }
         }
 
+        /**
+         * Sets the camera location
+         */
         private void UpdateCameraPosition()
         {
             transform.position = objLoc + relativeLocation;
         }
 
+        /**
+         * Gets the most recent objectToStare location
+         */
         private void UpdateObjLoc()
         {
             if (objHasDifferentLocation)
@@ -74,22 +81,12 @@ namespace Gameplay.Camera
             objLoc = objectToStare.transform.position;
         }
 
+        /**
+         * Sets the Camera rotation to look at the objectToStare
+         */
         private void UpdateCamRotation()
         {
             transform.rotation = Quaternion.LookRotation(objLoc - transform.position);
-        }
-
-        /**
-         * Didn't intent to not use. Since it's not working as intended, I won't use it.
-         */
-        private void UpdateObjRot()
-        {
-            if (objHasDifferentLocation)
-            {
-                objRot = objLocation.GetRotation();
-                return;
-            }
-            objRot = objectToStare.transform.rotation;
         }
     }
 }
