4b56174343d1efd1a9c8b3150d1202a3f32e9940
DavidYeLuo <davidayeluo@gmail.com>
2022-07-23 13:01:29 -0700

    Cleaned up code for mesh.

--- TankGame/Assets/Scripts/Gameplay/Mesh/MatchPosition.cs
+++ TankGame/Assets/Scripts/Gameplay/Mesh/MatchPosition.cs
@@ -6,13 +6,11 @@ namespace Gameplay.Mesh
 {
     public class MatchPosition : MonoBehaviour
     {
-        // [SerializeField] private GameObject obj;
         [SerializeField] private Collider collider;
 
         private void FixedUpdate()
         {
             transform.position = collider.transform.position;
-            // transform.position = obj.transform.position;
         }
     }
 }
