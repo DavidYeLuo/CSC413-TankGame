9a024c725dba640f27dfe0f14f45e136a0870f07
DavidYeLuo <davidayeluo@gmail.com>
2022-07-23 09:35:37 -0700

    Refactored MainMenuManager into controller and driver.

---
+++ TankGame/Assets/Scripts/Systems/UI/MainMenuManagerDriver.cs
@@ -0,0 +1,66 @@
+using Systems.InputSystem;
+using UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace Systems.UI
+{
+    public class MainMenuManagerDriver : MonoBehaviour
+    {
+        [SerializeField] private SystemAsset systemAsset;
+        [SerializeField] private Navigable startPanel;
+        
+        [Header("Driver(s)")]
+        [SerializeField] private InputDriver inputDriver;
+        
+        [Header("For Debug purpose")] 
+        [SerializeField] private Navigable currentPanel;
+        [SerializeField] private Navigable previousPanel;
+        private void Start()
+        {
+            systemAsset.SetPlayerInputs(null); // resets the playerinputs
+            currentPanel = startPanel;
+            currentPanel.gameObject.SetActive(true);
+        }
+        
+        public void GotoPreviousPanel()
+        {
+            Navigate(previousPanel);
+        }
+        public void Navigate(Navigable panel)
+        {
+            currentPanel.Navigate(panel);
+            previousPanel = currentPanel;
+            currentPanel = panel;
+        }
+        public virtual void SaveSystemAsset()
+        {
+            InputDriver.Instance.SavePlayersControl();
+        }
+        public virtual void EnablePlayerJoin()
+        {
+            PlayerInputManager.instance.EnableJoining();
+        }
+
+        public virtual void DisablePlayerJoin()
+        {
+            PlayerInputManager.instance.DisableJoining();
+        }
+        
+        public virtual void ExitApplication()
+        {
+            Application.Quit();
+        }
+        
+        public virtual void LoadScene(string scene)
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+        public virtual void EnterGameplay()
+        {
+            // Switch scenes here.
+            InputController.SwitchMode(UserMode.Gameplay);
+        }
+    }
+}
