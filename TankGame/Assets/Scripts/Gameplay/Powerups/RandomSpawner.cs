5271f1ed10e6fc484414a122be2836ab4282bd9a
DavidYeLuo <davidayeluo@gmail.com>
2022-08-02 12:59:54 -0700

    Added a powerup spawner.

---
+++ TankGame/Assets/Scripts/Gameplay/Powerups/RandomSpawner.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomSpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> items;
+    [SerializeField] private List<GameObject> possiblePositions;
+    [SerializeField] private float spawnDelayInSeconds;
+    [SerializeField] private bool isSpawning;
+
+    [Header("Debug")] 
+    [SerializeField] private GameObject item;
+    [SerializeField] private Vector3 pos;
+    
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (isSpawning)
+        {
+            yield return new WaitForSeconds(spawnDelayInSeconds);
+            GenerateRandomItemAndPosition();
+            Instantiate(item, pos, Quaternion.identity);
+        }
+    }
+
+    private void GenerateRandomItemAndPosition()
+    {
+        item = items[Random.Range(0, items.Count - 1)];
+        pos = possiblePositions[Random.Range(0, possiblePositions.Count - 1)].transform.position;
+    }
+}
